0cd7b11c111fb80607f25d085ce2f49cf20a107c
adcerro <adcerro@uninorte.edu.co>
2024-04-24 22:17:00 -0500

    dashboard redesing

--- lib/views/coordinator_dashboard.dart
+++ lib/views/coordinator_dashboard.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
@@ -30,16 +32,17 @@ class _DashboardState extends State<CoordinatorDashboard> {
   }
 
   int _selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dashboard Coordinador"),
+        title: const Text("Dashboard Coordinador"),
         backgroundColor: Theme.of(context).primaryColor,
         foregroundColor: Theme.of(context).colorScheme.onPrimary,
         automaticallyImplyLeading: false,
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.exit_to_app),
+            icon: const Icon(Icons.exit_to_app),
             tooltip: 'Logout',
             onPressed: () {
               Navigator.pushNamedAndRemoveUntil(
@@ -77,79 +80,137 @@ class _DashboardState extends State<CoordinatorDashboard> {
 }
 
 class UserSupportManagement extends StatelessWidget {
+  const UserSupportManagement({super.key});
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(8),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          children: [
-            Text('Gestión de Soporte de Usuario',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            TextFormField(decoration: InputDecoration(labelText: 'ID')),
-            TextFormField(decoration: InputDecoration(labelText: 'Nombre')),
-            TextFormField(decoration: InputDecoration(labelText: 'Email')),
-            TextFormField(decoration: InputDecoration(labelText: 'Contraseña')),
-            ElevatedButton(onPressed: () {}, child: Text('Guardar Usuario'))
-          ],
-        ),
+    return ListView(children: [
+      ExpansionTile(
+        title: const Text('Añadir Usuario'),
+        childrenPadding: const EdgeInsets.all(8),
+        children: [
+          TextFormField(decoration: const InputDecoration(labelText: 'ID')),
+          TextFormField(decoration: const InputDecoration(labelText: 'Nombre')),
+          TextFormField(decoration: const InputDecoration(labelText: 'Email')),
+          TextFormField(
+              decoration: const InputDecoration(labelText: 'Contraseña')),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Guardar Usuario'))
+        ],
       ),
-    );
+      ExpansionTile(
+          title: const Text('Editar Usuario'),
+          childrenPadding: const EdgeInsets.all(8),
+          children: [
+            TextFormField(decoration: const InputDecoration(labelText: 'ID')),
+            TextFormField(
+                decoration: const InputDecoration(labelText: 'Nombre')),
+            TextFormField(
+                decoration: const InputDecoration(labelText: 'Email')),
+            TextFormField(
+                decoration: const InputDecoration(labelText: 'Contraseña')),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Guardar Cambios'))
+          ]),
+      ExpansionTile(
+          title: const Text('Eliminar Usuario'),
+          childrenPadding: const EdgeInsets.all(8),
+          children: [
+            TextFormField(decoration: const InputDecoration(labelText: 'ID')),
+            TextFormField(
+                decoration: const InputDecoration(labelText: 'Nombre')),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Eliminar Usuario'))
+          ])
+    ]);
   }
 }
 
 class ClientManagement extends StatelessWidget {
+  const ClientManagement({super.key});
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(8),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          children: [
-            Text('Gestión de Clientes',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            TextFormField(decoration: InputDecoration(labelText: 'ID')),
-            TextFormField(decoration: InputDecoration(labelText: 'Nombre')),
-            ElevatedButton(onPressed: () {}, child: Text('Guardar Cliente'))
-          ],
-        ),
+    return ListView(children: [
+      ExpansionTile(
+        title: const Text('Añadir Cliente'),
+        childrenPadding: const EdgeInsets.all(8),
+        children: [
+          TextFormField(decoration: const InputDecoration(labelText: 'ID')),
+          TextFormField(decoration: const InputDecoration(labelText: 'Nombre')),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Guardar Cliente'))
+        ],
       ),
-    );
+      ExpansionTile(
+          title: const Text('Editar Cliente'),
+          childrenPadding: const EdgeInsets.all(8),
+          children: [
+            TextFormField(decoration: const InputDecoration(labelText: 'ID')),
+            TextFormField(
+                decoration: const InputDecoration(labelText: 'Nombre')),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Guardar Cambios'))
+          ]),
+      ExpansionTile(
+          title: const Text('Eliminar Cliente'),
+          childrenPadding: const EdgeInsets.all(8),
+          children: [
+            TextFormField(decoration: const InputDecoration(labelText: 'ID')),
+            TextFormField(
+                decoration: const InputDecoration(labelText: 'Nombre')),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Eliminar Cliente'))
+          ])
+    ]);
   }
 }
 
 class WorkReportEvaluation extends StatelessWidget {
+  const WorkReportEvaluation({super.key});
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(8),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          children: [
-            Text('Evaluación de Informes de Trabajo',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            ListTile(
-              title: Text('Usuario 1'),
-              subtitle: RatingBar.builder(
-                initialRating: 3,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                itemBuilder: (context, _) =>
-                    Icon(Icons.star, color: Colors.amber),
-                onRatingUpdate: (rating) {
-                  print(rating);
-                },
-              ),
-            ),
-          ],
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: [
+        const Text('Evaluación de Informes de Trabajo',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        TextFormField(
+            decoration: const InputDecoration(labelText: 'Id del Informe')),
+        Container(
+          alignment: Alignment.center,
+          child: const Text('Preview del Informe'),
         ),
-      ),
+        const Text('Evaluación',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        Center(
+            child: RatingBar.builder(
+          initialRating: 3,
+          minRating: 1,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          itemCount: 5,
+          itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+          itemBuilder: (context, _) => Icon(Icons.star, color: Colors.amber),
+          onRatingUpdate: (rating) {},
+        ))
+      ],
     );
   }
 }
