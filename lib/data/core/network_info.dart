02ad2a48c129c175810fd7349f30ab91f659421b
adcerro <adcerro@uninorte.edu.co>
2024-06-01 11:10:42 -0500

    moved to a more recent version of http

---
+++ lib/data/core/network_info.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:loggy/loggy.dart';
+
+/// The following code was taken from
+/// https://github.com/augustosalazar/f_web_retool_hive/blob/main/lib/data/core/network_info.dart
+class NetworkInfo with UiLoggy {
+  late StreamSubscription<InternetStatus> _internetStatusSubscription;
+  late StreamController<bool> _controller;
+
+  Stream<bool> get stream => _controller.stream;
+
+  NetworkInfo() {
+    _controller = StreamController<bool>.broadcast();
+  }
+
+  void openStream() {
+    _internetStatusSubscription = InternetConnection()
+        .onStatusChange
+        .listen(_internetStatusSubscriptionListener);
+  }
+
+  void closeStream() {
+    _internetStatusSubscription.cancel();
+  }
+
+  void _addValue(bool value) {
+    _controller.add(value);
+  }
+
+  void dispose() {
+    _controller.close();
+  }
+
+  Future<bool> isConnected() async {
+    return await InternetConnection().hasInternetAccess;
+  }
+
+  void _internetStatusSubscriptionListener(InternetStatus status) {
+    _addValue(status == InternetStatus.connected);
+  }
+}
